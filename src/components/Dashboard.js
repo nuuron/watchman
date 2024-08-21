385714c56e115395abc5191cfe57e6352f9753a1
Anuran Saikia <77756834+nuuron@users.noreply.github.com>
2024-08-22 02:29:39 +0530

    Initial commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Instagram Dashboard</h1>
+      <p>This will display Instagram data once connected.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
